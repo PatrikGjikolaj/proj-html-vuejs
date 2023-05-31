4f8e5e8aafdb2e36559f12b60db6d8685a1d6e21
PatrikGjikolaj <123626448+PatrikGjikolaj@users.noreply.github.com>
2023-05-31 11:04:24 +0200

    Completato AppMainVerbatim

--- src/components/AppMainVerbatim.vue
+++ src/components/AppMainVerbatim.vue
@@ -1,9 +1,13 @@
 <script>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Navigation, Pagination, Autoplay, EffectCoverflow } from 'swiper';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import "swiper/css/effect-coverflow";
 
 export default {
     data() {
         return {
-            i: 0,
             testimonials: [
                 {
                     pic: "../src/assets/testimonial-avata-01.jpg",
@@ -13,47 +17,104 @@ export default {
                 },
                 {
                     pic: "../src/assets/testimonial-avata-02.jpg",
-                    title: "Cloud Storage",
-                    description: "You can store online lessons via online cloud or download to your device and revise lessons on the way",
-                    link: "Get Free Quote ->"
+                    name: "MINA HOLLACE",
+                    title: "High level of efficency and scientific teaching methods",
+                    job: "/Freelancer",
+                    description: "I am free to learn at my on space, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.",
                 },
                 {
                     pic: "../src/assets/testimonial-avata-03.jpg",
-                    title: "Multimedia Channels",
-                    description: "We provide study materials with various formats: video, audio, slides, doc, prints, books and applications.",
-                    link: "Start now ->"
+                    name: "FLORENCE THEMES",
+                    job: "/Multimedia Admin",
+                    title: "It's a choice of quality for people with special needs",
+                    description: "I'm very strict person so I require everything to be organized and neat. Then, I'll be able to make things right and shine. MaxCoach guys just got me",
                 },
                 {
                     pic: "../src/assets/testimonial-avata-04.jpg",
-                    title: "Multimedia Channels",
-                    description: "We provide study materials with various formats: video, audio, slides, doc, prints, books and applications.",
-                    link: "Start now ->"
+                    name: "MADLEY PONDOR",
+                    job: "IT Specialist",
+                    title: "Professional team of specialists and passionate mentors ar rech",
+                    description: "I need to get a certification for English proficiency and MAxCoach is my best choice. Their tutors are smart and professional when dealing with students",
+
                 }
             ],
         }
     },
-    mounted() {
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
 
+    setup() {
+        return {
+            modules: [Navigation, Pagination, Autoplay, EffectCoverflow],
+        }
     }
 }
 </script>
 
 <template>
-    <div class="container-fluid py-5">
+    <div class="container-fluid py-5 bg_grey1">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h6>GREAT WORDS ABOUT MAXCOACH</h6>
+                <h4>Our top learners' verbatim</h4>
+            </div>
+        </div>
+        <div class="row py-5">
+            <div class="col-12">
+                <swiper :slidesPerView="3" :spaceBetween="50" :pagination="true" :mousewheel="true" :effect="'coverflow'"
+                    :grabCursor="true" :centeredSlides="true" :coverflowEffect="{
+                        rotate: 10,
+                        stretch: 0,
+                        depth: 100,
+                        modifier: 0.1,
+                        slideShadows: true,
+                    }" :modules="modules" class="mySwiper">
+                    <template v-for="testimonial in testimonials">
+                        <swiper-slide>
+                            <div class="row card border-0 p-2">
+                                <div class="list-group list-group-flush">
+                                    <div class="col-12">
+                                        <h4>{{ testimonial.title }}</h4>
+                                        <p>{{ testimonial.description }}</p>
+                                    </div>
+                                    <div class="list-group-item d-flex">
+                                        <div class="col-2">
+                                            <img :src="testimonial.pic" alt="" class="rounded-circle img-fluid">
+                                        </div>
+                                        <div class="col-10">
+                                            <h5>{{ testimonial.name }}</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </swiper-slide>
+                    </template>
+                </swiper>
+            </div>
+        </div>
         <div class="container">
             <div class="row">
-                <div class="col-12 text-center">
-                    <h6>GREAT WORDS ABOUT MAXCOACH</h6>
-                    <h4>Enjoy Our Companionship</h4>
+                <div class="col-2 pt-5">
+                    <img src="../assets/maxcoach-shape-01.png" alt="">
                 </div>
-                <div class="row">
-                    <div class="col-4 offset-4">
-
-                    </div>
+                <div class="col-8 text-center">
+                    <h5>Start today for getting Online Certification</h5>
+                    <h3>You can be your own guiding star with our help!</h3>
+                    <button type="button" class="btn btn-success my-3 px-5">View all courses -></button>
+                </div>
+                <div class="col-2 pt-5">
+                    <img src="../assets/maxcoach-shape-02.png" alt="">
                 </div>
             </div>
         </div>
+
     </div>
 </template>
 
-<style></style>
+<style>
+.bg_grey1 {
+    background-color: #f5f7fa;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ import './style.css'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import './style.css'
 
 createApp(App).mount('#app')
